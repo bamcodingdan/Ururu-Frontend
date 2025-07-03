c251bcc2fb4a18d431778368384f3826e7c848bd
1000hyehyang <ducogus12@gmail.com>
2025-07-03 18:06:41 +0900

    fix: 프로필 수정 관련 기획 반영
    - 프로필 이미지 업로드 막힘
    - 주문/배송 컴포넌트 삭제

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MyPageLayout } from '@/components/mypage/MyPageLayout';
 import { ProfileCard } from '@/components/mypage/ProfileCard';
-import { OrderStatus } from '@/components/mypage/OrderStatus';
 import { BeautyProfileSummary } from '@/components/mypage/beauty-profile';
 import { MobileSidebarList } from '@/components/mypage/MobileSidebarList';
 import { useMyPage } from '@/hooks/useMyPage';
@@ -16,9 +15,6 @@ export default function MyPage() {
 
       {/* 뷰티 프로필 요약 카드 */}
       {hasBeautyProfile && <BeautyProfileSummary summaryInfo={summaryInfo} />}
-
-      {/* 주문/배송 카드 */}
-      <OrderStatus />
 
       {/* 모바일/태블릿: 사이드바 리스트 */}
       <MobileSidebarList />
