7ba9bc57899d2a1d47ab20db7d6420715b6d16ee
1000hyehyang <ducogus12@gmail.com>
2025-07-04 11:07:38 +0900

    feat: 카테고리 랭킹 페이지 UI 구현

---
+++ src/app/ranking/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RankingLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ src/components/ranking/index.ts
@@ -0,0 +1,3 @@
+export { RankingProductCard } from './RankingProductCard';
+export { CategorySelector, type CategoryOption } from './CategorySelector';
+export { RankingProductGrid } from './RankingProductGrid';
